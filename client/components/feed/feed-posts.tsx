2e7c7449a16f7383b2f1f96223babe7f97dae97f
md-rejoyan-islam <rejoyanislam0014@gmail.com>
2026-04-06 03:47:25 +0600

    image feature modify

--- client/components/feed/feed-posts.tsx
+++ client/components/feed/feed-posts.tsx
@@ -715,6 +715,7 @@ export function FeedPostCard({
               alt="Post"
               width={600}
               height={400}
+              unoptimized
               className="w-full h-auto rounded-md object-cover"
             />
           </div>
